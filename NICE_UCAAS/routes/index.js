f7874d155847a8f8c7ead92b526dd92aa5f675a3
freakcap <kulkarni.pranav123@gmail.com>
2020-11-21 11:35:01 +0530

    Added skeletal code

--- NICE_UCAAS/routes/index.js
+++ NICE_UCAAS/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var bodyParser  = require('body-parser');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/',function(req, res, next){
+  if(!req) {
+    res.sendStatus(404);
+  }
+  if(req.body.platform == "zoom"){
+    res.redirect('/zoom/send',307);
+  }
+  else if(req.body.platform == "teams"){
+    res.redirect('/teams',307);
+  }
+  else{
+    res.sendStatus(404);
+  }
 });
 
 module.exports = router;
