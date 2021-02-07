4fbd6901ade1571613274dd57b154da623265c8c
NirajSarode <nirajsarode40@gmail.com>
2021-02-07 17:34:11 +0530

    teams auth

---
+++ frontend/src/components/auth/teamsLogin.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, NavLink as RouterNavLink } from 'react-router-dom';
+import {
+  Button,
+  Collapse,
+  Container,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Jumbotron
+} from 'reactstrap';
+import { UserAgentApplication } from 'msal';
+// import '@fortawesome/fontawesome-free/css/all.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+import { config } from '../config/teamsConfig';
+import ErrorMessage from '../apis/teams/ErrorMessage';
+import { normalizeError, getUserProfile } from '../apis/teams/MSUtils';
+
+class teamsLogin extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+      error: null,
+      isAuthenticated: false,
+      user: {}
+    };
+
+    this.userAgentApplication = new UserAgentApplication({
+      auth: {
+        clientId: config.clientId,
+        redirectUri: config.redirectUri
+      },
+      cache: {
+        cacheLocation: "sessionStorage",
+        storeAuthStateInCookie: true
+      }
+    });
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  async login() {
+    try {
+      console.log("HERE-1");
+
+      await this.userAgentApplication.loginPopup(
+        {
+          scopes: config.scopes,
+          prompt: "select_account"
+        });
+        console.log("HERE");
+      const user = await getUserProfile(this.userAgentApplication, config.scopes);
+      this.setState({
+        isAuthenticated: true,
+        user: {
+          displayName: user.displayName,
+          email: user.mail || user.userPrincipalName
+        },
+        error: null
+      });
+      console.log(user);
+    }
+    catch (err) {
+      this.setState({
+        isAuthenticated: false,
+        user: {},
+        error: normalizeError(err)
+      });
+    }
+  }
+
+  logout() {
+    this.userAgentApplication.logout();
+  }
+
+  render() {
+    let error = null;
+    if (this.state.error) {
+      error = <ErrorMessage
+        message={this.state.error.message}
+        debug={this.state.error.debug} />;
+    }
+    return (
+      <Router>
+        <div>
+          <Container>
+            {error}
+            <Route exact path="/"
+              render={() =>
+                <Jumbotron>
+                  {this.state.isAuthenticated
+                    ? <div>
+                      <h4>Welcome {this.state.user.displayName}!</h4>
+                    </div>
+                    : <Button color="primary" onClick={() => this.login()}>Click here to sign in</Button>
+                  }
+                </Jumbotron>
+              } />
+          </Container>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default teamsLogin;
---
+++ frontend/src/components/config/teamsConfig.js
@@ -0,0 +1,7 @@
+export const config = {
+    clientId: '31a4d57b-e578-48e7-849c-7d8d188abaa4',
+    redirectUri: 'http://localhost:5000',
+    scopes: [
+        'user.read'
+    ]
+};
