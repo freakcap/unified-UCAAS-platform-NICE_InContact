19ed16ebdb2e5c22cc5bcab5c40eb2411f554847
NirajSarode <nirajsarode40@gmail.com>
2021-03-17 11:40:09 +0530

    logout feature

--- frontend/src/components/config/slackConfig.js
+++ frontend/src/components/config/slackConfig.js
@@ -1,6 +1,6 @@
 
 export const config = {
     clientId: '1788828310994.1785891210678',
-    redirectUri: 'https://8613565fe0c4.ngrok.io',
+    redirectUri: 'https://db66ac01a871.ngrok.io',
     scopes: 'users:read'
 };
